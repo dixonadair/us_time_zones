78958b79e1179d56a1374f72f72eaba09df632d4
Dixon Adair <dixonadairv@gmail.com>
2015-07-18 16:03:00 -0700

    v 1.0.6

--- lib/us_time_zones.rb
+++ lib/us_time_zones.rb
@@ -11,6 +11,14 @@ require "httparty"
 
 class USTZones
 	def self.getZone(lat, lng)
+		p lat.class
+		p lng.class
+		if lat.class != String
+			lat = lat.to_s
+		end
+		if lng.class != String
+			lng = lng.to_s
+		end
 		url = "https://maps.googleapis.com/maps/api/geocode/json?latlng=#{lat},#{lng}&key=AIzaSyBgUT0t3K3y9cKJgITa8X1O7uGYjguNZT4"
 		response = HTTParty.get(url).to_json
 		response = JSON.parse(response)
@@ -28,11 +36,11 @@ end
 # 53.37734988622845, -169.504588281682
 
 class TZtest
-	def self.test(lat,lng)
+	def self.test(lat, lng)
 		if HAWAII_ALEUTIAN.include?([lat,lng])
-			p "includes!"
+			p "includes bla bla!"
 		else
-			p "does not include!"
+			p "does not include bla bla bla!"
 		end
 	end
 end
