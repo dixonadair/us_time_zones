7131119df4e3ee9a2a886177f613e1d1279744f4
Dixon Adair <dixonadairv@gmail.com>
2015-07-18 15:17:17 -0700

    version 1.0.0

--- Gemfile
+++ Gemfile
@@ -1,4 +1,8 @@
 source 'https://rubygems.org'
 
+gem 'httparty'
+
+gem 'json'
+
 # Specify your gem's dependencies in us_time_zones.gemspec
 gemspec
--- lib/us_time_zones.rb
+++ lib/us_time_zones.rb
@@ -1,11 +1,20 @@
 require "us_time_zones/version"
 
-module UsTimeZones
-	def self.getZone(lat)
-		if lat == 1
-			return "yes"
+require "json"
+require "httparty"
+
+class USTZones
+	def self.getZone(lat, lng)
+		url = "https://maps.googleapis.com/maps/api/geocode/json?latlng=#{lat},#{lng}&key=AIzaSyBgUT0t3K3y9cKJgITa8X1O7uGYjguNZT4"
+		response = HTTParty.get(url).to_json
+		response = JSON.parse(response)
+
+		if response["results"] != []
+			p "Error"
+		elsif response["results"][0]["formatted_address"].include?("USA")
+			p "You're in the USA"
 		else
-			return "no"
+			p "You're not in the USA"
 		end
 	end
 end
