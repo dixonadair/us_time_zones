97a8cfd857a2bd10d9929e88baa4712f0f182d90
Dixon Adair <dixonadairv@gmail.com>
2015-07-19 15:56:59 -0700

    version 2.0.0

--- lib/us_time_zones.rb
+++ lib/us_time_zones.rb
@@ -1,10 +1,11 @@
 require "us_time_zones/version"
 
-require_relative "us_time_zones/tz_coord_files/hawaii_aleutian"
-# require "us_time_zones/tz_coord_files/alaska"
-# require "us_time_zones/tz_coord_files/pacific"
-# require "us_time_zones/tz_coord_files/central"
-# require "us_time_zones/tz_coord_files/eastern"
+require_relative "us_time_zones/tz_coord_files/hawaii_aleutian_1"
+require_relative "us_time_zones/tz_coord_files/hawaii_aleutian_2"
+require_relative "us_time_zones/tz_coord_files/central"
+require_relative "us_time_zones/tz_coord_files/eastern"
+require_relative "us_time_zones/tz_coord_files/pacific"
+require_relative "us_time_zones/tz_coord_files/alaska"
 
 require "json"
 require "httparty"
@@ -16,6 +17,13 @@ require 'geo_ruby/gpx'        # GPX data
 require 'geo_ruby/kml'        # KML data
 require 'geo_ruby/georss'     # GeoRSS
 require 'geo_ruby/geojson'    # GeoJSON
+
+alaska = GeoRuby::SimpleFeatures::Polygon.from_coordinates([ALASKA])
+hawaii_aleutian_1 = GeoRuby::SimpleFeatures::Polygon.from_coordinates([HAWAII_ALEUTIAN_1])
+hawaii_aleutian_2 = GeoRuby::SimpleFeatures::Polygon.from_coordinates([HAWAII_ALEUTIAN_2])
+central = GeoRuby::SimpleFeatures::Polygon.from_coordinates([CENTRAL])
+eastern = GeoRuby::SimpleFeatures::Polygon.from_coordinates([EASTERN])
+pacific = GeoRuby::SimpleFeatures::Polygon.from_coordinates([PACIFIC])
 
 class USTZones
 	def self.getZone(lat, lng)
@@ -51,10 +59,33 @@ class TZtest
 end
 
 class TZ
-	def self.pointInPolygon?(shape_coords_arr, point)
-		polygon = GeoRuby::SimpleFeatures::Polygon.from_coordinates([[[53.33434512824876, -172.3672895105823], [53.37738931378584, -169.5046181489856], [53.37734988622845, -169.504588281682], [52.52095253013979, -169.5124025591721]]])
-		p polygon
-		p point
-		puts polygon.contains_point?(point)
+	def self.whichTZ?(point)
+		pointObject = GeoRuby::SimpleFeatures::Point.from_x_y(point[0], point[1])
+		url = "https://maps.googleapis.com/maps/api/geocode/json?latlng=#{point[0].to_s},#{point[1].to_s}&key=AIzaSyBgUT0t3K3y9cKJgITa8X1O7uGYjguNZT4"
+		response = HTTParty.get(url).to_json
+		response = JSON.parse(response)
+
+		if response["results"] == []
+			p "Error"
+		elsif response["results"][0]["formatted_address"].include?("USA")
+			p "You're in the USA"
+			if eastern.contains_point?(pointObject)
+				p "Eastern Time Zone"
+			elsif central.contains_point?(pointObject)
+				p "Central Time Zone"
+			elsif pacific.contains_point?(pointObject)
+				p "Pacific Time Zone"
+			elsif hawaii_aleutian_1.contains_point?(pointObject) || hawaii_aleutian_2.contains_point?(pointObject)
+				p "Hawaii-Aleutian Time Zone"
+			elsif alaska.contains_point?(pointObject)
+				p "Alaska Time Zone"
+			else
+				p "Mountain Time Zone"
+			end	
+		else
+			p "You're not in the USA"
+		end
+
+		# puts polygon.contains_point?(point)
 	end
 end
